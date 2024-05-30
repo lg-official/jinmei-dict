0d027cdf812b51db0afef51c3fc48ee125f89a8d
Tomoki Kimura <art.detonation@gmail.com>
2024-05-30 02:38:15 +0000

    add tss and llm temporarily for SF反映依頼_20240526②

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-python jinmei-dict.py '../data/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv' '../data/mecab-ipadic-neologd/mecab-user-dict-seed.20200130/mecab-user-dict-seed.20200130.csv' '../data/addon/addon.csv' '../data/addon/user_dict.csv' '../data/addon/tss_sei.csv' '../data/addon/tss_mei.csv'
+python jinmei-dict.py '../data/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv' '../data/mecab-ipadic-neologd/mecab-user-dict-seed.20200130/mecab-user-dict-seed.20200130.csv' '../data/addon/addon.csv' '../data/addon/user_dict.csv' '../data/addon/tss_sei.csv' '../data/addon/tss_mei.csv' '../data/addon/llm.csv'
