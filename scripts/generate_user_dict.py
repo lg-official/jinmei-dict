5a41872f6c437505aa313433caefe85d92d9843e
Tomoki Kimura <art.detonation@gmail.com>
2024-06-03 01:39:00 +0000

    add generate_user_dict

---
+++ scripts/generate_user_dict.py
@@ -0,0 +1,27 @@
+import csv
+
+# 入力ファイル名と出力ファイル名を指定
+input_file = '../data/addon/user_dict_seed.csv'
+output_file = '../data/addon/user_dict_generated.csv'
+
+# CSVファイルを読み込み、指定の形式に変換する関数
+
+
+def convert_csv(input_file, output_file):
+    with open(input_file, mode='r', encoding='utf-8') as infile, \
+            open(output_file, mode='w', encoding='utf-8', newline='') as outfile:
+
+        reader = csv.reader(infile, skipinitialspace=True)
+        writer = csv.writer(outfile)
+
+        for row in reader:
+            type = row[0]
+            if type == '姓' or type == '名':
+                for yomi in row[2:]:
+                    converted_row = [row[1], '*', '*', '*', '名詞',
+                                     '固有名詞', '人名', type, '*', '*', '*', yomi, yomi]
+                    writer.writerow(converted_row)
+
+
+# 関数を実行
+convert_csv(input_file, output_file)
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
-python jinmei-dict.py '../data/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv' '../data/mecab-ipadic-neologd/mecab-user-dict-seed.20200130/mecab-user-dict-seed.20200130.csv' '../data/addon/addon.csv' '../data/addon/user_dict.csv' '../data/addon/tss_sei.csv' '../data/addon/tss_mei.csv' '../data/addon/llm.csv'
+python generate_user_dict.py 
+python jinmei-dict.py '../data/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv' '../data/mecab-ipadic-neologd/mecab-user-dict-seed.20200130/mecab-user-dict-seed.20200130.csv' '../data/addon/addon.csv' '../data/addon/user_dict.csv' '../data/addon/user_dict_generated.csv' '../data/addon/tss_sei.csv' '../data/addon/tss_mei.csv'
