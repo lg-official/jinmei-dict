66f662b681d0458590c618d77988b1e2491e6751
Tomoki Kimura <tkimura@legacy-jp.com>
2024-06-08 01:08:30 +0000

    add 寿始

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,3 +3,5 @@ set -e
 
 python generate_user_dict.py 
 python jinmei-dict.py '../data/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv' '../data/mecab-ipadic-neologd/mecab-user-dict-seed.20200130/mecab-user-dict-seed.20200130.csv' '../data/addon/addon.csv' '../data/addon/user_dict.csv' '../data/addon/user_dict_generated.csv' '../data/addon/tss_sei.csv' '../data/addon/tss_mei.csv'
+
+./copy.sh
